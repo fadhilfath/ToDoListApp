c9ca8bca8a555d410054028f5cc428d48bcaa93a
fadhilsuhendi <fadhilfathurrahman@rocketmail.com>
2016-01-09 15:58:11 +0000

    adding styling and modified controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,32 @@
+class ItemsController < ApplicationController
+    
+    def index
+       @item = Item.new
+       @all_items = Item.order(created_at: :desc).all
+    end
+    
+    def new
+       @item = Item.new 
+    end
+    
+    def show 
+       @all_items = Item.all 
+    end
+    
+    def create
+        @item = Item.new(item_params)
+        if @item.save
+           flash[:success] = "You have successfully added item!"    
+           redirect_to root_path
+        else
+           render 'new' 
+        end
+    end
+    
+    
+    private
+    def item_params
+       params.require(:item).permit(:item) 
+    end
+    
+end
